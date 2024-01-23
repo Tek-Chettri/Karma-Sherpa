2dfde3ca9e1308969f5ff7acbb2342da4280d4c2
KarmaPempa <karmasherpa1761@gmail.com>
2024-01-23 15:16:19 +0530

    clickup project

--- clickup/clickup.js
+++
@@ -1,9 +0,0 @@
-// Add your JavaScript code here
-
-document.getElementById('signupLink').addEventListener('click', function() {
-    document.getElementById('signupPopup').style.display = 'block';
-});
-
-function closePopup() {
-    document.getElementById('signupPopup').style.display = 'none';
-}
